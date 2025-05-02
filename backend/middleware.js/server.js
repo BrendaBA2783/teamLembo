b6e17d603bae88969231eef36321dd0f3ab5d757
BrendaBA2783 <161029967+BrendaBA2783@users.noreply.github.com>
2025-05-01 22:39:49 -0500

    Cambios a user validation y server
    Sin id personalizado

--- backend/middleware.js/server.js
+++ backend/middleware.js/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
@@ -7,193 +7,167 @@ app.use(express.json());
 app.use(cors());
 
 // Configuración de conexión a la base de datos
-async function main() { 
-  const db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '', 
-      database: 'lembo'
-  });
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', 
+    database: 'lembo'
+});
 
-  // Conectar a la base de datos
+// Conectar a la base de datos
+db.connect(err => {
+  if (err) {
+    console.log('Error de conexión a la base de datos:', err);
+    return;
+  }
   console.log('Conectado a la base de datos');
-  
+});
 
-  // Ruta para obtener el nuevo ID automático
-  app.get('/nuevo-id', async (req, res) => {
-    try {
-      const [filas] = await db.query('SELECT user_id FROM users ORDER BY user_id DESC LIMIT 1');
-      const ultimo = filas[0]?.user_id || 'USER-0000';
-      const nuevoId = generarNuevoId(ultimo);
+// Ruta para agregar un usuario
+app.post('/users', (req, res) => {
+  const{user_type, user_document_type, user_name, user_last_name, user_identification_number, user_phone, user_email, confirmation_email } = req.body;
 
-      console.log("Nuevo ID generado:", nuevoId);
-      res.json({ id_generado: nuevoId });
-    } catch (error) {
-      console.error('Error al generar ID:', error);
-      res.status(500).json({ error: 'Error interno' });
+  db.query (
+    'INSERT INTO users (user_type, user_document_type, user_name, user_last_name, user_identification_number, user_phone, user_email, confirmation_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+
+    [user_type, user_document_type, user_name, user_last_name, user_identification_number, user_phone, user_email, confirmation_email],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: 'Error al insertar datos' });
+        return;
+      }
+      res.status(200).json({ message: 'Usuario agregado' });
     }
+  );
+});
+// Ruta para obtener los usuarios
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM users ORDER BY user_name DESC';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Error al obtener los usuarios');
+      return;
+    }
+    res.json(result);
   });
-  // Ruta para agregar un usuario
-  app.post('/users', (req, res) => {
-    const{user_id, user_type, user_document_type, user_name, user_last_name, user_document_number, user_phone, user_email } = req.body;
+});
 
-    console.log("Datos recibidos para el nuevo usuario:", req.body);
-    
-    db.query (
-      'INSERT INTO users (user_id, user_type, user_document_type, user_name, user_last_name, user_document_number, user_phone, user_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+// Ruta para agregar un cultivo
+app.post('/crop', (req, res) => {
+  const{crop_id, crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description } = req.body;
 
-      [user_id, user_type, user_document_type, user_name, user_last_name, user_document_number, user_phone, user_email],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al insertar datos' });
-          return;
-        }
-        res.status(200).json({ message: 'Usuario registrado' });
-      }
-    );
-  });
-  // Ruta para obtener los usuarios
-  app.get('/users', (req, res) => {
-    const query = 'SELECT * FROM users ORDER BY user_name DESC';
-    db.query(query, (err, result) => {
+  db.query (
+    'INSERT INTO crop (crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description) VALUES (?, ?, ?, ?, ?, ?, ?)',
+
+    [crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description],
+    (err, result) => {
       if (err) {
-        res.status(500).send('Error al obtener los usuarios');
+        res.status(500).json({ error: 'Error al insertar datos' });
         return;
       }
-      res.json(result);
-    });
+      res.status(200).json({ message: 'Cultivo agregado' });
+    }
+  );
+});
+// Ruta para obtener los cultivos
+app.get('/crop', (req, res) => {
+  const query = 'SELECT * FROM crop ORDER BY crop_name DESC';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Error al obtener los cultivos');
+      return;
+    }
+    res.json(result);
   });
+});
 
-  // Ruta para agregar un cultivo
-  app.post('/crop', (req, res) => {
-    const{crop_id, crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description } = req.body;
+// Ruta para agregar un ciclo de cultivo
+app.post('/cycle', (req, res) => {
+  const{cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description} = req.body;
 
-    db.query (
-      'INSERT INTO crop (crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description) VALUES (?, ?, ?, ?, ?, ?, ?)',
+  db.query (
+    'INSERT INTO cycle (cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description) VALUES (?, ?, ?, ?, ?, ?, ?)',
 
-      [crop_type, crop_location, crop_size, crop_name, crop_state, crop_image, crop_description],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al insertar datos' });
-          return;
-        }
-        res.status(200).json({ message: 'Cultivo agregado' });
-      }
-    );
-  });
-  // Ruta para obtener los cultivos
-  app.get('/crop', (req, res) => {
-    const query = 'SELECT * FROM crop ORDER BY crop_name DESC';
-    db.query(query, (err, result) => {
+    [cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description],
+    (err, result) => {
       if (err) {
-        res.status(500).send('Error al obtener los cultivos');
+        res.status(500).json({ error: 'Error al insertar datos' });
         return;
       }
-      res.json(result);
-    });
+      res.status(200).json({ message: 'Ciclo agregado' });
+    }
+  );
+});
+// Ruta para obtener los ciclos
+app.get('/cycle', (req, res) => {
+  const query = 'SELECT * FROM cycle ORDER BY cycle_name DESC';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Error al obtener los ciclos');
+      return;
+    }
+    res.json(result);
   });
+});
 
-  // Ruta para agregar un ciclo de cultivo
-  app.post('/cycle', (req, res) => {
-    const{cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description} = req.body;
 
-    db.query (
-      'INSERT INTO cycle (cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description) VALUES (?, ?, ?, ?, ?, ?, ?)',
+// Ruta para agregar un insumo
+app.post('/supplies', (req, res) => {
+  const{supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description} = req.body;
 
-      [cycle_location, cycle_name, cycle_status, cycle_news, cycle_start_date, cycle_end_date, cycle_description],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al insertar datos' });
-          return;
-        }
-        res.status(200).json({ message: 'Ciclo agregado' });
-      }
-    );
-  });
-  // Ruta para obtener los ciclos
-  app.get('/cycle', (req, res) => {
-    const query = 'SELECT * FROM cycle ORDER BY cycle_name DESC';
-    db.query(query, (err, result) => {
+  db.query (
+    'INSERT INTO supplies (supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+
+    [supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description],
+    (err, result) => {
       if (err) {
-        res.status(500).send('Error al obtener los ciclos');
+        res.status(500).json({ error: 'Error al insertar datos' });
         return;
       }
-      res.json(result);
-    });
+      res.status(200).json({ message: 'Insumo agregado' });
+    }
+  );
+});
+// Ruta para obtener los insumos
+app.get('/supplies', (req, res) => {
+  const query = 'SELECT * FROM supplies ORDER BY supplies_name DESC';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Error al obtener los insumos');
+      return;
+    }
+    res.json(result);
   });
+});
 
+// Ruta para agregar un sensor
+app.post('/sensor', (req, res) => {
+  const{sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description} = req.body;
 
-  // Ruta para agregar un insumo
-  app.post('/supplies', (req, res) => {
-    const{supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description} = req.body;
+  db.query (
+    'INSERT INTO sensor (sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
 
-    db.query (
-      'INSERT INTO supplies (supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-
-      [supplies_id, crop_cycle_id, crop_id, supplies_type, supplies_cuantity, supplies_name, supplies_state, supplies_unit_value, supplies_total_value, supplies_measure, supplies_description],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al insertar datos' });
-          return;
-        }
-        res.status(200).json({ message: 'Insumo agregado' });
-      }
-    );
-  });
-  // Ruta para obtener los insumos
-  app.get('/supplies', (req, res) => {
-    const query = 'SELECT * FROM supplies ORDER BY supplies_name DESC';
-    db.query(query, (err, result) => {
+    [sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description],
+    (err, result) => {
       if (err) {
-        res.status(500).send('Error al obtener los insumos');
+        res.status(500).json({ error: 'Error al insertar datos' });
         return;
       }
-      res.json(result);
-    });
+      res.status(200).json({ message: 'Sensor agregado' });
+    }
+  );
+});
+// Ruta para obtener los sensores
+app.get('/sensor', (req, res) => {
+  const query = 'SELECT * FROM sensor ORDER BY sensor_name DESC';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Error al obtener los sensores');
+      return;
+    }
+    res.json(result);
   });
-
-  // Ruta para agregar un sensor
-  app.post('/sensor', (req, res) => {
-    const{sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description} = req.body;
-
-    db.query (
-      'INSERT INTO sensor (sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-
-      [sensor_id, sensor_measure, sensor_type, sensor_scan_time, sensor_name, sensor_status, sensor_image, sensor_description],
-      (err, result) => {
-        if (err) {
-          res.status(500).json({ error: 'Error al insertar datos' });
-          return;
-        }
-        res.status(200).json({ message: 'Sensor agregado' });
-      }
-    );
-  });
-  // Ruta para obtener los sensores
-  app.get('/sensor', (req, res) => {
-    const query = 'SELECT * FROM sensor ORDER BY sensor_name DESC';
-    db.query(query, (err, result) => {
-      if (err) {
-        res.status(500).send('Error al obtener los sensores');
-        return;
-      }
-      res.json(result);
-    });
-  });
-
-  app.listen(3000, () => {
-    console.log ('Servidor corriendo en http://localhost:3000');
-  });
-}
-
-// Generar nuevo ID de usuario
-function generarNuevoId(user_id) {
-  const num = parseInt(user_id.split('-')[1]) + 1;
-  return `USER-${num.toString().padStart(4, '0')}`;
-}
-
-main();
-
+});
 
 // Ruta para agregar asociacion
 /* app.post('/asociacion', (req, res) => {
@@ -212,6 +186,12 @@ main();
     }
   );
 }); */
+
+app.listen(3000, () => {
+  console.log ('Servidor corriendo en http://localhost:3000');
+});
+
+
 // Ruta para obtener los usuarios
 /* app.get('/users', (req, res) => {
   const query = 'SELECT * FROM usuarios ORDER BY id_usuario DESC';
--- backend/middleware.js/user-validation-register.js
+++ backend/middleware.js/user-validation-register.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     const userForm = document.querySelector('.main__container--userForm');
 
     // Selección de los inputs
     const userType = document.querySelector('.main__form-field--userType'); 
-    const userId = document.querySelector('.main__form-field--userId'); 
     const userDocumentType = document.querySelector('.main__form-field--userDocumentType'); 
-    const userName = document.querySelector('.main__form-field--userName');
-    const userState = document.querySelector('.main__form-field--userState');
+    const userName = document.querySelector('.main__form-field--userName'); 
     const userLastName = document.querySelector('.main__form-field--userLastName'); 
     const userIdentificationNumber = document.querySelector('.main__form-field--userIdentificationNumber');
     const userCellPhone = document.querySelector('.main__form-field--userCellPhone'); 
@@ -15,26 +13,15 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // Objeto que se actualizará con cada input
     const userData = {
-        user_id: '',
         user_type: '',
         user_document_type: '',
-        user_document_number: '',
         user_name: '',
         user_last_name: '',
-        user_email: '',
-        confirmation_email: '',
+        user_identification_number: '',
         user_phone: '',
-        user_state: ''
+        user_email: '',
+        confirmation_email: ''
     };
-
-    try {
-        const res = await fetch('http://localhost:3000/nuevo-id');
-        const data = await res.json();
-        document.querySelector('.main__form-field--userId').value = data.id_generado;
-        userData.user_id = data.id_generado;
-      } catch (error) {
-        console.error('Error al generar ID:', error);
-    }
 
     // Validación de caracteres especiales
     function hasSpecialChars(str) {
@@ -52,6 +39,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     userLastName.addEventListener('input', (e) => {
+        // Expresión regular que solo permite letras y espacios
         const regex = /[^a-zA-ZáéíóúÁÉÍÓÚñÑ ]/g;
         if (regex.test(e.target.value)) {
             showError('El apellido del usuario solo puede contener letras y espacios');
@@ -61,22 +49,20 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     userIdentificationNumber.addEventListener('input', (e) => {
         if (isNaN(e.target.value)) {
-            showError('El número de identificación solo permite números');
+            showError('El número de identificación debe contener solo números');
             e.target.value = e.target.value.replace(/[^0-9]/g, '');
         }
     });
 
     userCellPhone.addEventListener('input', (e) => {
         if (isNaN(e.target.value)) {
-            showError('El número de teléfono solo permite números');
+            showError('El número de teléfono debe contener solo números');
             e.target.value = e.target.value.replace(/[^0-9]/g, '');
         }
     });
 
     // Actualizar el objeto userData en tiempo real
     userType.addEventListener('input', readText);
-    userId.addEventListener('input', readText);
-    userState.addEventListener('input', readText);
     userDocumentType.addEventListener('input', readText);
     userName.addEventListener('input', readText);
     userLastName.addEventListener('input', readText);
@@ -92,14 +78,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             userData.user_document_type = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userName')) {
             userData.user_name = e.target.value;
-        } else if (e.target.classList.contains('main__form-field--userId')) {
-            userData.user_id = e.target.value;
-        } else if (e.target.classList.contains('main__form-field--userState')) {
-            userData.user_state = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userLastName')) {
             userData.user_last_name = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userIdentificationNumber')) {
-            userData.user_document_number = e.target.value;
+            userData.user_identification_number = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userCellPhone')) {
             userData.user_phone = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userEmail')) {
@@ -113,16 +95,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Envío del formulario
     userForm.addEventListener('submit', async (event) => {
         event.preventDefault();
-        console.log('Intento enviarme');   
 
         try {
             // Verificar que todos los campos esten completos
             for (const key in userData) {
-                if (key !== 'user_id' && userData[key].trim() === '') {
-                    console.log(`Campo vacío: ${key}`);
-                    throw new Error(`El campo ${key} es obligatorio`);
+                if (userData[key].trim() === '') {
+                    throw new Error('Todos los campos son obligatorios');
                 }
             }
+
             // Validaciones
             if (hasSpecialChars(userData.user_name)) {
                 throw new Error('El nombre no puede contener caracteres especiales');
@@ -132,7 +113,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 throw new Error('Los apellidos no pueden contener caracteres especiales');
             }
 
-            if (!/^\d+$/.test(userData.user_document_number)) {
+            if (!/^\d+$/.test(userData.user_identification_number)) {
                 throw new Error('Número de identificación inválido');
             }
 
@@ -144,7 +125,6 @@ document.addEventListener('DOMContentLoaded', async () => {
                 throw new Error('Los correos no coinciden');
             }
 
-            console.log('Enviando solicitud POST...')
             const response = await fetch('http://localhost:3000/users', {
                 method: 'POST',
                 headers: {
@@ -154,31 +134,16 @@ document.addEventListener('DOMContentLoaded', async () => {
             });
 
             if (!response.ok) {
-                console.log('Error en la respuesta: ', response.status);
                 throw new Error('Error en la conexión con el servidor');
             }
 
             const result = await response.json();
+            console.log('Usuario registrado:', result);
             showSuccess('El formulario ha sido completado correctamente');
-
-        } catch (error) {
-            console.log('Error al procesar la solicitud: ',error);
-            showError(error.message);
-        }
+            } catch (error) {
+                showError(error.message);
+            }
     });
-
-    function showSuccess(message) {
-        console.log('Mensaje de éxito');
-        const success = document.createElement('P');
-        success.textContent = message;
-        success.classList.add('correct');
-        userForm.appendChild(success);
-        setTimeout(() => {
-            success.remove();
-            console.log('Redirigiendo...');
-            window.location.href = 'register-credentials.html';
-        }, 2000);
-    }
 
     function showError(message) {
         console.log(message);
@@ -186,6 +151,17 @@ document.addEventListener('DOMContentLoaded', async () => {
         error.textContent = message;
         error.classList.add('error');
         userForm.appendChild(error);
-        setTimeout(() => error.remove(), 2000);
+        setTimeout(() => error.remove(), 4000);
+    }
+
+    function showSuccess(message) {
+        const success = document.createElement('P');
+        success.textContent = message;
+        success.classList.add('correct');
+        userForm.appendChild(success);
+        setTimeout(() => {
+            success.remove();
+            window.location.href = 'register-credentials.html';
+        }, 1000);
     }
 });
