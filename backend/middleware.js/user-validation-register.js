c7f78f83109840c305ffce44f5e157e2f6a28df1
BrendaBA2783 <161029967+BrendaBA2783@users.noreply.github.com>
2025-05-01 19:56:06 -0500

    Id personalizado users bd
    Id personalizado para users, error en validacion de exito, las demas validaciones estan bien

--- backend/middleware.js/user-validation-register.js
+++ backend/middleware.js/user-validation-register.js
@@ -1,10 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const userForm = document.querySelector('.main__container--userForm');
 
     // Selección de los inputs
     const userType = document.querySelector('.main__form-field--userType'); 
+    const userId = document.querySelector('.main__form-field--userId'); 
     const userDocumentType = document.querySelector('.main__form-field--userDocumentType'); 
-    const userName = document.querySelector('.main__form-field--userName'); 
+    const userName = document.querySelector('.main__form-field--userName');
+    const userState = document.querySelector('.main__form-field--userState');
     const userLastName = document.querySelector('.main__form-field--userLastName'); 
     const userIdentificationNumber = document.querySelector('.main__form-field--userIdentificationNumber');
     const userCellPhone = document.querySelector('.main__form-field--userCellPhone'); 
@@ -13,15 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Objeto que se actualizará con cada input
     const userData = {
+        user_id: '',
         user_type: '',
         user_document_type: '',
+        user_document_number: '',
         user_name: '',
         user_last_name: '',
-        user_identification_number: '',
-        user_phone: '',
         user_email: '',
-        confirmation_email: ''
+        confirmation_email: '',
+        user_phone: '',
+        user_state: ''
     };
+
+    try {
+        const res = await fetch('http://localhost:3000/nuevo-id');
+        const data = await res.json();
+        document.querySelector('.main__form-field--userId').value = data.id_generado;
+        userData.user_id = data.id_generado;
+      } catch (error) {
+        console.error('Error al generar ID:', error);
+    }
 
     // Validación de caracteres especiales
     function hasSpecialChars(str) {
@@ -39,7 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     userLastName.addEventListener('input', (e) => {
-        // Expresión regular que solo permite letras y espacios
         const regex = /[^a-zA-ZáéíóúÁÉÍÓÚñÑ ]/g;
         if (regex.test(e.target.value)) {
             showError('El apellido del usuario solo puede contener letras y espacios');
@@ -49,20 +61,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     userIdentificationNumber.addEventListener('input', (e) => {
         if (isNaN(e.target.value)) {
-            showError('El número de identificación debe contener solo números');
+            showError('El número de identificación solo permite números');
             e.target.value = e.target.value.replace(/[^0-9]/g, '');
         }
     });
 
     userCellPhone.addEventListener('input', (e) => {
         if (isNaN(e.target.value)) {
-            showError('El número de teléfono debe contener solo números');
+            showError('El número de teléfono solo permite números');
             e.target.value = e.target.value.replace(/[^0-9]/g, '');
         }
     });
 
     // Actualizar el objeto userData en tiempo real
     userType.addEventListener('input', readText);
+    userId.addEventListener('input', readText);
+    userState.addEventListener('input', readText);
     userDocumentType.addEventListener('input', readText);
     userName.addEventListener('input', readText);
     userLastName.addEventListener('input', readText);
@@ -78,10 +92,14 @@ document.addEventListener('DOMContentLoaded', () => {
             userData.user_document_type = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userName')) {
             userData.user_name = e.target.value;
+        } else if (e.target.classList.contains('main__form-field--userId')) {
+            userData.user_id = e.target.value;
+        } else if (e.target.classList.contains('main__form-field--userState')) {
+            userData.user_state = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userLastName')) {
             userData.user_last_name = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userIdentificationNumber')) {
-            userData.user_identification_number = e.target.value;
+            userData.user_document_number = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userCellPhone')) {
             userData.user_phone = e.target.value;
         } else if (e.target.classList.contains('main__form-field--userEmail')) {
@@ -95,15 +113,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Envío del formulario
     userForm.addEventListener('submit', async (event) => {
         event.preventDefault();
+        console.log('Intento enviarme');   
 
         try {
             // Verificar que todos los campos esten completos
             for (const key in userData) {
-                if (userData[key].trim() === '') {
-                    throw new Error('Todos los campos son obligatorios');
+                if (key !== 'user_id' && userData[key].trim() === '') {
+                    console.log(`Campo vacío: ${key}`);
+                    throw new Error(`El campo ${key} es obligatorio`);
                 }
             }
-
             // Validaciones
             if (hasSpecialChars(userData.user_name)) {
                 throw new Error('El nombre no puede contener caracteres especiales');
@@ -113,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error('Los apellidos no pueden contener caracteres especiales');
             }
 
-            if (!/^\d+$/.test(userData.user_identification_number)) {
+            if (!/^\d+$/.test(userData.user_document_number)) {
                 throw new Error('Número de identificación inválido');
             }
 
@@ -125,6 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error('Los correos no coinciden');
             }
 
+            console.log('Enviando solicitud POST...')
             const response = await fetch('http://localhost:3000/users', {
                 method: 'POST',
                 headers: {
@@ -134,16 +154,31 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             if (!response.ok) {
+                console.log('Error en la respuesta: ', response.status);
                 throw new Error('Error en la conexión con el servidor');
             }
 
             const result = await response.json();
-            console.log('Usuario registrado:', result);
             showSuccess('El formulario ha sido completado correctamente');
-            } catch (error) {
-                showError(error.message);
-            }
+
+        } catch (error) {
+            console.log('Error al procesar la solicitud: ',error);
+            showError(error.message);
+        }
     });
+
+    function showSuccess(message) {
+        console.log('Mensaje de éxito');
+        const success = document.createElement('P');
+        success.textContent = message;
+        success.classList.add('correct');
+        userForm.appendChild(success);
+        setTimeout(() => {
+            success.remove();
+            console.log('Redirigiendo...');
+            window.location.href = 'register-credentials.html';
+        }, 2000);
+    }
 
     function showError(message) {
         console.log(message);
@@ -151,17 +186,6 @@ document.addEventListener('DOMContentLoaded', () => {
         error.textContent = message;
         error.classList.add('error');
         userForm.appendChild(error);
-        setTimeout(() => error.remove(), 4000);
-    }
-
-    function showSuccess(message) {
-        const success = document.createElement('P');
-        success.textContent = message;
-        success.classList.add('correct');
-        userForm.appendChild(success);
-        setTimeout(() => {
-            success.remove();
-            window.location.href = 'register-credentials.html';
-        }, 1000);
+        setTimeout(() => error.remove(), 2000);
     }
 });
